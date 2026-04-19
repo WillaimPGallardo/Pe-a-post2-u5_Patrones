e064b2e7f61e2ba71c5d90ca0470fc0f68cc5247
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-19 17:58:00 -0500

    COMMIT 4
    feat: creación de DTOs para entrada y salida de datos

--- biblioteca-api/src/dto/LibroRequestDTO.java
+++ biblioteca-api/src/dto/LibroRequestDTO.java
@@ -1,3 +1,20 @@
+package com.universidad.patrones.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class LibroRequestDTO {
-    
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String autor;
+
+    @NotBlank
+    private String isbn;
+
+    private Integer anioPublicacion;
+    private String categoria;
 }
