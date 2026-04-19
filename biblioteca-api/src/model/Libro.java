70327cb81e63e5d8d9d4ffc0a6d7966123aa307a
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-19 17:56:46 -0500

    COMMIT 3
    feat: creación de entidad Libro

--- biblioteca-api/src/model/Libro.java
+++ biblioteca-api/src/model/Libro.java
@@ -1,3 +1,21 @@
+package com.universidad.patrones.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private Integer anioPublicacion;
+    private String categoria;
 }
