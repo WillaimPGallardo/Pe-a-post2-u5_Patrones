524cb92b75234057d50ae7387f596e751c152bbb
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-19 17:58:41 -0500

    COMMIT 5
    "feat: implementación de mapper para conversión DTO-Entity"

--- biblioteca-api/src/dto/LibroResponseDTO.java
+++ biblioteca-api/src/dto/LibroResponseDTO.java
@@ -1,3 +1,15 @@
+package com.universidad.patrones.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
 public class LibroResponseDTO {
-    
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private Integer anioPublicacion;
+    private String categoria;
 }
--- biblioteca-api/src/mapper/LibroMapper.java
+++ biblioteca-api/src/mapper/LibroMapper.java
@@ -1,3 +1,23 @@
+package com.universidad.patrones.mapper;
+
+import com.universidad.patrones.dto.*;
+import com.universidad.patrones.model.Libro;
+
 public class LibroMapper {
-    
+
+    public static Libro toEntity(LibroRequestDTO dto) {
+        return new Libro(null, dto.getTitulo(), dto.getAutor(),
+                dto.getIsbn(), dto.getAnioPublicacion(), dto.getCategoria());
+    }
+
+    public static LibroResponseDTO toDTO(Libro libro) {
+        return new LibroResponseDTO(
+                libro.getId(),
+                libro.getTitulo(),
+                libro.getAutor(),
+                libro.getIsbn(),
+                libro.getAnioPublicacion(),
+                libro.getCategoria()
+        );
+    }
 }
