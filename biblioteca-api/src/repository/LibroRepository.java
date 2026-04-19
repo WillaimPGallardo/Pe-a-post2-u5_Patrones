b1cce4e5e7c6024d74e4f47607f4b27aae013408
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-19 17:59:55 -0500

    COMMIT 6
    "feat: implementación de repositorio"

--- biblioteca-api/src/repository/LibroRepository.java
+++ biblioteca-api/src/repository/LibroRepository.java
@@ -1,3 +1,7 @@
-public class LibroRepository {
-    
+package com.universidad.patrones.repository;
+
+import com.universidad.patrones.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
 }
